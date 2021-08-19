c3bb7e333fb7f1b5794e133de84fc907f39fd309
Tolulope Alegbeleye <76182917+thollulope@users.noreply.github.com>
2021-08-19 23:46:02 +0100

    add script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var firstname, lastname;
+
+firstname = 'Tolulope';
+lastname ='Alegbeleye';
+
+var guess = prompt('my name is Alegbeleye Tolulope. What is yours?')
+console.log( firstname + '' + lastname );
